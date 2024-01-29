71c9aafb667f4f1252d1be4e2a7a8f1409eeb621
GuilhermeBrennand <g.brennand@hotmail.com>
2024-01-28 21:50:53 -0300

    Criando componente MenuOptions e adicionando icones

--- src/app/modules/apps/menu/MenuPage.tsx
+++ src/app/modules/apps/menu/MenuPage.tsx
@@ -1,24 +1,34 @@
+import { KTCard, KTCardBody } from "../../../../_metronic/helpers"
 import { Offers } from "./components/Offers"
 import { PaymentMethod } from "./components/PaymentMethod"
+import { MenuOptions } from "./components/list/MenuOptions"
 import { ShoppingCart } from "./components/table/ShoppingCart"
 
 const MenuPage = () => {
     return (
-        <div className="d-flex flex-column flex-row-fluid">
-            <div className="d-flex flex-row-auto w-600px  flex-center">
-                <div className="card card-flush bg-body " id="kt_pos_form">
-                    <div className="card-header pt-5">
-                        <h3 className="card-title fw-bold text-gray-800 fs-2qx">Current Order</h3>
+        <div className="d-flex flex-column flex-xl-row">
+            <div className="d-flex flex-row-fluid me-xl-9 mb-10 mb-xl-0">
+                <KTCard>
+                    <KTCardBody>
+                        <MenuOptions />
+                    </KTCardBody>
+                </KTCard>
+            </div>
+            <div className="flex-row-auto w-xl-450px">
+                <div className="d-flex flex-row-auto w-600px  flex-center">
+                    <div className="card card-flush bg-body " id="kt_pos_form">
+                        <div className="card-header pt-5">
+                            <h3 className="card-title fw-bold text-gray-800 fs-2qx">Current Order</h3>
 
-                        <div className="card-toolbar">
-                            <a href="#" className="btn btn-light-primary fs-4 fw-bold py-4">Clear All</a>
+                            <div className="card-toolbar">
+                                <a href="#" className="btn btn-light-primary fs-4 fw-bold py-4">Clear All</a>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="card-body pt-0">
-                        <ShoppingCart className={'card-body pt-0'} />
-                        <Offers className="d-flex flex-stack bg-success rounded-3 p-6 mb-11" />
-                        <PaymentMethod />
+                        <div className="card-body pt-0">
+                            <ShoppingCart className={'card-body pt-0'} />
+                            <Offers className="d-flex flex-stack bg-success rounded-3 p-6 mb-11" />
+                            <PaymentMethod />
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/app/modules/apps/menu/components/list/MenuOptions.tsx
@@ -0,0 +1,70 @@
+const MenuOptions = () => {
+    return (
+        <ul className="nav nav-pills d-flex justify-content-between nav-pills-custom gap-3 mb-6" role="tablist">
+            <li className="nav-item mb-3 me-0" role="presentation">
+                <a className="nav-link nav-link-border-solid btn btn-outline btn-flex btn-active-color-primary flex-column flex-stack pt-9 pb-7 page-bg show active" data-bs-toggle="pill" href="#kt_pos_food_content_1" aria-selected="true" role="tab">
+                    <div className="nav-icon mb-3">
+                        <img src="src/_metronic/assets/media/svg/food-icons/spaghetti.svg" className="w-50px" alt="" />
+                    </div>
+                    <div className="">
+                        <span className="text-gray-800 fw-bold fs-2 d-block">Lunch</span>
+                        <span className="text-gray-500 fw-semibold fs-7">8 Options</span>
+                    </div>
+                </a>
+            </li>
+
+            <li className="nav-item mb-3 me-0" role="presentation">
+                <a className="nav-link nav-link-border-solid btn btn-outline btn-flex btn-active-color-primary flex-column flex-stack pt-9 pb-7 page-bg" data-bs-toggle="pill" href="#kt_pos_food_content_2" aria-selected="false" role="tab" >
+                    <div className="nav-icon mb-3">
+                        <img src="src/_metronic/assets/media/svg/food-icons/salad.svg" className="w-50px" alt="" />
+                    </div>
+                    <div className="">
+                        <span className="text-gray-800 fw-bold fs-2 d-block">Salad</span>
+                        <span className="text-gray-500 fw-semibold fs-7">14 Salads</span>
+                    </div>
+                </a>
+            </li>
+
+            <li className="nav-item mb-3 me-0" role="presentation">
+                <a className="nav-link nav-link-border-solid btn btn-outline btn-flex btn-active-color-primary flex-column flex-stack pt-9 pb-7 page-bg " data-bs-toggle="pill" href="#kt_pos_food_content_3" aria-selected="false" role="tab">
+                    <div className="nav-icon mb-3">
+                        <img src="src/_metronic/assets/media/svg/food-icons/cheeseburger.svg" className="w-50px" alt="" />
+                    </div>
+
+                    <div className="">
+                        <span className="text-gray-800 fw-bold fs-2 d-block">Burger</span>
+                        <span className="text-gray-500 fw-semibold fs-7">5 Burgers</span>
+                    </div>
+                </a>
+            </li>
+
+            <li className="nav-item mb-3 me-0" role="presentation">
+                <a className="nav-link nav-link-border-solid btn btn-outline btn-flex btn-active-color-primary flex-column flex-stack pt-9 pb-7 page-bg " data-bs-toggle="pill" href="#kt_pos_food_content_4" aria-selected="false" role="tab">
+                    <div className="nav-icon mb-3">
+                        <img src="src/_metronic/assets/media/svg/food-icons/coffee.svg" className="w-50px" alt="" />
+                    </div>
+
+                    <div className="">
+                        <span className="text-gray-800 fw-bold fs-2 d-block">Coffee</span>
+                        <span className="text-gray-500 fw-semibold fs-7">7 Beverages</span>
+                    </div>
+                </a>
+            </li>
+
+            <li className="nav-item mb-3 me-0" role="presentation">
+                <a className="nav-link nav-link-border-solid btn btn-outline btn-flex btn-active-color-primary flex-column flex-stack pt-9 pb-7 page-bg " data-bs-toggle="pill" href="#kt_pos_food_content_5" aria-selected="false" role="tab">
+                    <div className="nav-icon mb-3">
+                        <img src="src/_metronic/assets/media/svg/food-icons/cheesecake.svg" className="w-50px" alt="" />
+                    </div>
+
+                    <div >
+                        <span className="text-gray-800 fw-bold fs-2 d-block">Dessert</span>
+                        <span className="text-gray-500 fw-semibold fs-7">8 Desserts</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    )
+}
+
+export { MenuOptions }
