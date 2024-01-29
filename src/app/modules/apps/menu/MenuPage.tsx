e05958917980bd49fc44169d278cd68f6b74ec9c
GuilhermeBrennand <g.brennand@hotmail.com>
2024-01-28 21:07:19 -0300

    Criando o menupage

---
+++ src/app/modules/apps/menu/MenuPage.tsx
@@ -0,0 +1,16 @@
+
+const MenuPage = () => {
+
+    return (
+        <div className="card card-flush bg-body " id="kt_pos_form">
+            <div className="card-header pt-5">
+                <h3 className="card-title fw-bold text-gray-800 fs-2qx">Current Order</h3>
+
+                <div className="card-toolbar">
+                    <a href="#" className="btn btn-light-primary fs-4 fw-bold py-4">Clear All</a>
+                </div>
+            </div>
+        </div>)
+}
+
+export { MenuPage }
